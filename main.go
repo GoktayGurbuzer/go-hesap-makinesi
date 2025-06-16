a10a07bdde2d383ab7591a2d9d4826e6e3cbb52f
Göktay Gürbüzer <goktaygurbuzer@gmail.com>
2025-06-16 14:06:05 +0200

    Update main.go

--- main.go
+++ main.go
@@ -4,43 +4,55 @@ import (
 	"errors"
 	"fmt"
 )
-
+// Verileri float64 olarak alıyoruz, float64 ve varsa error tipinde geri gönderiyoruz
 func topla(birinci, ikinci float64) (float64, error) {
+	// Sayılar 0'a eşit veya küçük mü? kontrolü. 
 	if birinci <= 0 || ikinci <= 0 {
 		return 0, errors.New("Sayılar 0 olamaz")
 	}
 
+	// Sorun yoksa, iki değeri topluyoruz, sonuc ve hata kontrolü ile geri gönderiyoruz
 	return birinci + ikinci, nil
 }
 
+// Verileri float64 olarak alıyoruz, float64 ve varsa error tipinde geri gönderiyoruz
 func cikar(birinci, ikinci float64) (float64, error) {
+	// Sayılar 0'a eşit veya küçük mü? kontrolü. 
 	if birinci <= 0 || ikinci <= 0 {
 		return 0, errors.New("Sayılar 0 olamaz")
 	}
 
+	// Sorun yoksa, iki değeri çıkartıyoruz, sonuc ve hata kontrolü ile geri gönderiyoruz
 	return birinci - ikinci, nil
 }
 
+// Verileri float64 olarak alıyoruz, float64 ve varsa error tipinde geri gönderiyoruz
 func carp(birinci, ikinci float64) (float64, error) {
+	// Sayılar 0'a eşit veya küçük mü? kontrolü. 
 	if birinci <= 0 || ikinci <= 0 {
 		return 0, errors.New("Sayılar 0 olamaz")
 	}
+	
+	// Sorun yoksa, iki değeri çarpıyoruz, sonuc ve hata kontrolü ile geri gönderiyoruz
 	return birinci * ikinci, nil
 }
 
+// Verileri float64 olarak alıyoruz, float64 ve varsa error tipinde geri gönderiyoruz
 func bol(birinci, ikinci float64) (float64, error) {
+	// Sayılar 0'a eşit veya küçük mü? kontrolü. 
 	if birinci <= 0 || ikinci <= 0 {
 		return 0, errors.New("Sayılar 0 olamaz")
 	}
 
+	// Sorun yoksa, iki değeri bölüyoruz, sonuc ve hata kontrolü ile geri gönderiyoruz
 	return birinci / ikinci, nil
 }
 
 func main() {
-	var a, b float64
-	var islem string
-	var sonuc float64
-	var err error
+	var a, b float64 // Alacağımız değerler için değişken adını ve tipini belirtiyoruz
+	var islem string // Kullanıcının hangi işlemi seçtiğiniz string olarak alıyoruz
+	var sonuc float64 // Sonucu atayacağımız değişkenimiz
+	var err error // Hata için kullanacağımız değişkenimiz.
 
 	fmt.Println("İlk Sayıyı Girin ")
 	fmt.Scanln(&a)
@@ -70,5 +82,6 @@ func main() {
 		return
 	}
 
+	// İlk Sayı: 4, İkinci Sayı: 5, İşlem: / = "Girilen değerler: 4.00 / 5.00 = 0.80
 	fmt.Printf("Girilen değerler: %.2f %s %.2f = %.2f\n", a, islem, b, sonuc)
 }
